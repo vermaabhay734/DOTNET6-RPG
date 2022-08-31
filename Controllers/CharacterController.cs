2ada288916bed46644d3142880158a72bc5ba4c6
abhay734 <vermaabhay734@gmail.com>
2022-08-31 17:30:15 +0530

    New controller & GET a new Character

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DOTNET_RPG.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static Character knight = new Character();
+
+        [HttpGet]
+        public ActionResult<Character> Get(){
+            return Ok(knight);
+        }
+    }
+}
--- Models/RpgClass.cs
+++ Models/RpgClass.cs
@@ -1,5 +1,8 @@
+using System.Text.Json.Serialization;
+
 namespace DOTNET_RPG.Models
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum RpgClass
     {
         Knight = 1,
