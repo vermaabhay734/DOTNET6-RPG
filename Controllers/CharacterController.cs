4b68d24007fac3d19b70821c2bda4eaf560a81f8
abhay734 <vermaabhay734@gmail.com>
2022-09-26 15:04:29 +0530

    Delete a Character

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -24,6 +24,17 @@ namespace DOTNET_RPG.Controllers
             return Ok(await _characterService.GetAllCharacters());
         }
         
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> Delete(int id){
+
+            var response = await _characterService.DeleteCharacter(id);
+            if(response.Data == null)
+            {
+                return NotFound(response);
+            }
+            return Ok(response);
+        }
+
         [HttpGet("{id}")]
         public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> GetSingle(int id){
             return Ok(_characterService.GetCharacterById(id));
