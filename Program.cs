6835095dd98ecd976080fa3b5a739a2241f346d6
abhay734 <vermaabhay734@gmail.com>
2022-09-26 23:08:22 +0530

    ConnectionString & Adding the DbContext

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 global using DOTNET_RPG.Models;
+using DOTNET_RPG.Data;
 using DOTNET_RPG.Services.CharacterService;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
